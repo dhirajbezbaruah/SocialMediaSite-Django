23629779617929fe4ea56f52ecf00becdbab11ca
Dhiraj Bezbaruah <37962935+dhirajbezbaruah@users.noreply.github.com>
2018-09-09 13:07:20 +0530

    initial

---
+++ posts/models.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.urls import reverse
+from django.db import models
+
+#import misaka
+#import mistune
+
+from groups.models import  Group
+
+try:
+    from django.contrib.auth import get_user_model
+    User = settings.AUTH_USER_MODEL
+except ImportError:
+    from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    message = models.TextField()
+    message_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="posts",null=True, blank=True)
+
+    def __str__(self):
+        return self.message
+
+    def save(self, *args, **kwargs):
+        self.message_html =(self.message)
+        #html_str = mistune.markdown(md_str)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse(
+            "posts:single",
+            kwargs={
+                "username": self.user.username,
+                "pk": self.pk
+            }
+        )
+
+    class Meta:
+        ordering = ["-created_at"]
+        unique_together = ["user", "message"]
